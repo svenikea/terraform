9e95dc60d3a795205a262a4ee28d254e0181af97
Ky.Nguyen <ky.nguyen@sts.saigontechnology>
2023-01-05 11:05:43 +0700

    update acm module

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,8 +1,25 @@
 resource "aws_acm_certificate" "domain_certificate" {
-    domain_name               = var.project_domain
+    count                     = var.route53_enable != false ? 1 : var.new_acm != false ? 1 : 0
+    domain_name               = var.project_sub_domain != null ? "${var.project_sub_domain}.${var.project_domain}" : var.project_domain
     validation_method         = var.validation_method
 
     private_key               = var.validation_method == "NONE" ? var.private_key : null
     certificate_body          = var.validation_method == "NONE" ? var.certificate_body : null
     certificate_chain         = var.validation_method == "NONE" ? var.certificate_chain : null
+}
+
+resource "aws_route53_record" "current_record" {
+    count                     = var.route53_enable != false ? 1 : 0
+    allow_overwrite           = true
+    name                      = tolist(aws_acm_certificate.domain_certificate[0].domain_validation_options)[0].resource_record_name
+    records                   = [tolist(aws_acm_certificate.domain_certificate[0].domain_validation_options)[0].resource_record_value]
+    type                      = tolist(aws_acm_certificate.domain_certificate[0].domain_validation_options)[0].resource_record_type
+    zone_id                   = var.route53_enable != false ? data.aws_route53_zone.current_zone.id : var.new_acm != false ? data.aws_route53_zone.current_zone.id : "empty"
+    ttl                       = 60
+}
+
+resource "aws_acm_certificate_validation" "validate_acm" {
+    count                     = var.route53_enable != false ? 1 : 0
+    certificate_arn           = aws_acm_certificate.domain_certificate[0].arn
+    validation_record_fqdns   = [aws_route53_record.current_record[0].fqdn]
 }
--- modules/acm/variables.tf
+++ modules/acm/variables.tf
@@ -1,5 +1,10 @@
-variable "project_domain" {}
-variable "validation_method" {}
-variable "certificate_chain" {
-    default = null
-}
+variable "project_domain" { default = null }
+variable "project_sub_domain" { default = null }
+variable "validation_method" { default = null }
+variable "certificate_chain" { default = null }
+variable "private_key" { default = null }
+variable "certificate_body" { default = null }
+
+## If using Route 53 then use these resources else comment it
+variable "route53_enable" { default = null }
+variable "new_acm" { default = null }
